94943b4f46ee0eee6fc28fba87d66575e9ee469e
Roman <felixcon@gmail.com>
2013-03-23 13:38:32 +0400

    with export

--- src/controller/kaede_main_controller.erl
+++ src/controller/kaede_main_controller.erl
@@ -1,12 +1,13 @@
 -module(kaede_main_controller, [Req]).
--compile(export_all).
+%-compile(export_all).
+-export([before_/3,hello/2,test_auth/0]).
 
-before_("test_auth") ->
+before_("test_auth",_,_) ->
     case member_lib:require_login(Req) of
 		fail-> {redirect, "/member/login"};
 		{ok, Member} -> {ok, Member}
     end;
-before_(_) ->
+before_(_,_,_) ->
   		{ok, []}.
 
 % Заглушка
@@ -15,7 +16,7 @@ hello('GET', []) ->
 
 
 test_auth() ->
-   {ok,[]}
+   {ok,[]}.
 % Will list our topics or better give json with topics
 %list('GET', []) ->
 %    Greetings = boss_db:find(greeting, []),
