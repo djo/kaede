0b22bfba7a706fbd6af6573cb3469c78459b5a56
Alexander Kovalev <ass.kovalev@gmail.com>
2013-03-23 19:11:15 +0400

    comments for db initialization

--- src/lib/db_defaults.erl
+++ src/lib/db_defaults.erl
@@ -1,3 +1,9 @@
+%%% provides methods for initial database data
+%%% if you need some pre-defined data in your model's table
+%%% you should define a parameterless method
+%%% with the name similar to the model's name
+%%% and return constructed entity list
+
 -module(db_defaults).
 -compile(export_all).
 
