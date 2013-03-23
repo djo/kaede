5a4fe577926e4c8b4a6d2c93fdb69d8af24f23c4
Roman <felixcon@gmail.com>
2013-03-23 13:22:06 +0400

    working filter

--- src/controller/kaede_main_controller.erl
+++ src/controller/kaede_main_controller.erl
@@ -1,16 +1,21 @@
 -module(kaede_main_controller, [Req]).
 -compile(export_all).
 
-before_(_) ->
+before_("test_auth") ->
     case member_lib:require_login(Req) of
 		fail-> {redirect, "/member/login"};
 		{ok, Member} -> {ok, Member}
-    end.
+    end;
+before_(_) ->
+  		{ok, []}.
 
 % Заглушка
 hello('GET', []) ->
     {output, "<strong>Rahm says hello!</strong>"}.
 
+
+test_auth() ->
+   {ok,[]}
 % Will list our topics or better give json with topics
 %list('GET', []) ->
 %    Greetings = boss_db:find(greeting, []),
