c879517ffd48621d120c5a9047efc5718e8cf3bb
Andrew Djoga <andrew.djoga@gmail.com>
2013-04-22 20:53:16 +0300

    Style messages a bit

--- priv/static/script/views/topic/topicListItem.js
+++ priv/static/script/views/topic/topicListItem.js
@@ -2,22 +2,26 @@ define([
     'Backbone',
     'text!templates/topic/topicListItem.html'
 ], function (Backbone, TopicListItemTemplate) {
-    var TopicListItemView = Backbone.View.extend({
-	tagName: 'div',
-	className: 'topic-item',
-	events: {
-	    'click h4': 'openChat'
-	},
-        
-	render: function () {
-	    var self = this;
-	    var compiledTemplate = _.template(TopicListItemTemplate, self.model.toJSON());
-	    self.$el.html(compiledTemplate);
-	    return self;
-	},
-        openChat: function() {
-            this.trigger('chat:open', this);
-        }
-    });
-    return TopicListItemView;
+  var TopicListItemView = Backbone.View.extend({
+    tagName: 'div',
+    className: 'topic-item',
+
+    events: {
+      'click h4 a': 'openChat'
+    },
+
+    render: function () {
+      var self = this;
+      var compiledTemplate = _.template(TopicListItemTemplate, self.model.toJSON());
+      self.$el.html(compiledTemplate);
+      return self;
+    },
+
+    openChat: function (e) {
+      e.preventDefault();
+      this.trigger('chat:open', this);
+    }
+  });
+
+  return TopicListItemView;
 }); 
